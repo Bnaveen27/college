9e8a8089ba60f61e792f86b6e38006159219305c
Bnaveen27 <naveenbala2701@gmail.com>
2024-12-27 09:36:18 +0530

    nav hide

--- src/core/components/navbar/NavBar.js
+++ src/core/components/navbar/NavBar.js
@@ -15,10 +15,10 @@ const NavBar = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            {/* <Link to="/about">About</Link> */}
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            {/* <Link to="/contact">Contact</Link> */}
           </li>
         </ul>
       </nav>
